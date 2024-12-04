bbddf5c371a57a1a3aaa6bc64a142cf18060732c
Paulo Marconi <162889413+Obolinho47@users.noreply.github.com>
2024-12-03 22:49:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import bcrypt
 import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'chave_secreta'  # Para as mensagens flash
+app.secret_key = 'chave_secreta'
 
 db_config = {
     'host': 'localhost',
@@ -15,12 +15,10 @@ db_config = {
 def get_db_connection():
     return mysql.connector.connect(**db_config)
 
-# Página Inicial
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
-# Cadastro
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     if request.method == 'POST':
@@ -28,19 +26,16 @@ def cadastro():
         email = request.form['email']
         senha = request.form['senha']
 
-        # Hash da senha
         hashed_senha = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
 
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        # Verificar se o usuário já existe
         cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
         if cursor.fetchone():
             flash("Usuário já cadastrado!", "error")
             return redirect(url_for('cadastro'))
 
-        # Inserir novo usuário no banco
         query = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
         cursor.execute(query, (nome, email, hashed_senha))
         conn.commit()
@@ -49,11 +44,10 @@ def cadastro():
         conn.close()
 
         flash("Cadastro realizado com sucesso!", "success")
-        return redirect(url_for('login'))  # Redireciona para a página de login
+        return redirect(url_for('login'))  
 
     return render_template('cadastro.html')
 
-# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -70,19 +64,17 @@ def login():
 
         if result and bcrypt.checkpw(senha.encode('utf-8'), result[1].encode('utf-8')):
             nome = result[0]
-            return redirect(url_for('entrada', nome=nome))  # Vai para a tela de entrada
+            return redirect(url_for('entrada', nome=nome))  
         else:
             flash("Usuário ou senha incorretos!", "error")
             return redirect(url_for('login'))
 
     return render_template('login.html')
 
-# Tela de Entrada
 @app.route('/entrada')
 def entrada():
     nome = request.args.get('nome')
     return render_template('entrada.html', nome=nome)
 
-# Rodando o app
 if __name__ == '__main__':
     app.run(debug=True)
